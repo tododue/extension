95181e29d19bfda062181a11b0026ccdf5c3665b
Chris Grabda <ccg5437@g.rit.edu>
2021-02-20 19:40:57 -0500

    added McGraw Hill and ExpertTA information to manifest sites and content.js, created function to send website HTML and name of site to server

--- content.js
+++ content.js
@@ -1,27 +1,53 @@
 let URL = $.get(url);
 let PAGE = $(document.html);
+let SERVERLINK = 'https://tododue.com/api/upload';
 
 let landing = false;
 let loginPhrase = 'login';
+let platform = '';
 
-
+/**
+ * Determines which site to use and what keywords to look for
+ */
 if (URL.has('mycourses')) {
-    loginPhrase = 'Login'
+    loginPhrase = 'Login';
+    platform = 'RIT_MYCOURSES';
 }
 else if (URL.has('webwork')) {
     loginPhrase = 'Not logged in.';
+    platform = 'RIT_WEBWORK';
 }
 else if (URL.has('mheducation.com')) {
     loginPhrase = 'Sign In';
+    platform = 'MCGRAW_HILL';
 }
-else if (URL.has('PEARSON')) {
-    loginPhrase = 'X';
+else if (URL.has('theexpertta.com')) {
+    loginPhrase = 'Log In';
+    platform = 'EXPERT_TA';
 }
-else if (URL.has('EXPERT TA')) {
-    loginPhrase = 'X';
+
+/**
+ * Sends the page information to the website to parse
+ */
+function sendPage() {
+    $.ajax({
+        url: SERVERLINK,
+        type: 'POST',
+        data: {
+            page: PAGE,
+            platform: platform
+        },
+        success: (e) => {
+
+        },
+        error: (e) => {
+
+        }
+    });
 }
+
 
 
 if (!page.includes(loginPhrase)) {
-    //do stuff
+    sendPage();
 }
